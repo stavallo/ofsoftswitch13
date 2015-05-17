f3336cc52911cccf0b06d4f6ee4b6b141f3141a0
Luciano J Chaves <ljerezchaves@gmail.com>
2015-05-17 01:00:42 -0300

    Fixing the redefined cpp flag.

--- nbee_link/automake.mk
+++ nbee_link/automake.mk
@@ -10,6 +10,10 @@ noinst_LIBRARIES += nbee_link/libnbee_link.a
 nbee_link_libnbee_link_a_SOURCES = nbee_link/nbee_link.cpp \
 			nbee_link/nbee_link.h
 
+netpdldir = $(datadir)/openflow
+netpdl_DATA = customnetpdl.xml
+nbee_link_libnbee_link_a_CPPFLAGS = $(AM_CPPFLAGS) -DNETPDLDIR='"$(netpdldir)"'
+
 MAINTAINERCLEANFILES = Makefile.in aclocal.m4 config.guess config.sub config.h.in configure depcomp install-sh missing ltmain.sh *~ *.tar.*
 
 
